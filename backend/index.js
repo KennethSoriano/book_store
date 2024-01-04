8293215e67f777128886bc89446188778ea053f2
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2024-01-04 11:18:19 -0800

    frontend

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(
     cors({
         origin: 'http://localhost:3000',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: [],
+        allowedHeaders: ['Content-Type'],
     })
 );
 
