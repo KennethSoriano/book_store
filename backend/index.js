c1fb341a3108b17d1b192da2af4ebb2d650e6d16
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2024-01-03 20:41:13 -0800

    first http route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from "./config.js";
 
 const app = express()
 
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('hey girl')
+});
+
 app.listen(PORT, () => {
     console.log(`App on port: ${PORT}`)
 })
